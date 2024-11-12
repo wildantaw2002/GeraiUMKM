2ecc5d18f567f64165c0df4fecc1adbd7e7a2616
arigunawanj <arigunawanjatmiko@gmail.com>
2024-11-12 08:54:28 +0700

    Fixing Migration - Double Migration Delete - Env File Example - composer ipdate

--- database/migrations/2024_11_06_999999_add_active_status_to_users.php
+++ database/migrations/2024_11_06_999999_add_active_status_to_users.php
@@ -4,7 +4,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-class AddActiveStatusToUsers extends Migration
+return new class extends Migration
 {
     /**
      * Run the migrations.
@@ -32,4 +32,4 @@ class AddActiveStatusToUsers extends Migration
             $table->dropColumn('active_status');
         });
     }
-}
+};
